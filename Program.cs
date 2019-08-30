cba3e785f666c29e51b190176e61df46324b8348
Gledson Duarte <duarte.gledson@gmail.com>
2019-08-30 16:32:32 -0300

    Initial state

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 
 namespace Console_BookExamples
 {
@@ -9,51 +6,12 @@ namespace Console_BookExamples
     // A typical class is responsible for only one specific operation 
     internal class Journal
     {
-        private readonly List<string> entries = new List<string>();
-        private static int count = 0;
-
-        public int AddEntry(string text)
-        {
-            entries.Add($"{++count}: {text}");
-            return count;
-        }
-
-        public void RemoveEntry(int index)
-        {
-            entries.RemoveAt(index);
-        }
-
-        public override string ToString()
-        {
-            return string.Join(Environment.NewLine, entries);
-        }
-    }
-
-    internal class Persistence
-    {
-        public void SaveToFile(Journal j, string filename, bool overwrite = false)
-        {
-            if (overwrite || !File.Exists(filename))
-            {
-                File.WriteAllText(filename, j.ToString());
-            }
-        }
     }
 
     internal static class Demo
     {
-        private static void  Main(string[] args)
+        private static void Main(string[] args)
         {
-            var j = new Journal();
-            j.AddEntry("I cried today");
-            j.AddEntry("I ate a bug");
-            Console.WriteLine(j);
-            
-            var p = new Persistence();
-            const string filename = @"C:\Users\dg_gt\Downloads\journal.txt";
-            p.SaveToFile(j, filename, true);
-            Process.Start(filename);
-            
         }
     }
 }
