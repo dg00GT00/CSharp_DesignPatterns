0a20dbdcadad7459ffd6ebb33994a2517be73af8
Gledson Duarte <duarte.gledson@gmail.com>
2019-09-27 15:54:49 -0300

    Code example

--- Program.cs
+++ Program.cs
@@ -1,17 +1,111 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Console_BookExamples
 {
-    //************* Single responsibility principle *************//
-    // A typical class is responsible for only one specific operation 
-    internal class Journal
+    //************* Abstract Factory principle *************//
+    // - A factory method is a static method that creates objects
+    // - A factory can take care of object creation
+    // - A factory can be external or reside inside the object as an inner class
+    // - Hierarchies of factories can be used to create related objects
+    public interface IHotDrink
     {
+        void Consume();
+    }
+
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("This tea is nice but I'd prefer it with milk'");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("This coffee is sensational");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Put in a tea bag, boil water, pour {amount}ml, and lemon, enjoy!");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Grind some beans, boil water, pour {amount}ml, add cream and sugar");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        private List<Tuple<string, IHotDrinkFactory>> _factories = new List<Tuple<string, IHotDrinkFactory>>();
+
+        // Constructor using the reflection technique. It has much of the same ideas behind the ReflectData library which is 
+        // employed on the Typescript world.
+        public HotDrinkMachine()
+        {
+            foreach (var t in typeof(HotDrinkMachine).Assembly.GetTypes())
+            {
+                if (typeof(IHotDrinkFactory).IsAssignableFrom(t) && !t.IsInterface)
+                {
+                    _factories.Add(Tuple.Create(t.Name.Replace("Factory", string.Empty),
+                        (IHotDrinkFactory) Activator.CreateInstance(t)));
+                }
+            }
+        }
+
+        public IHotDrink MakeDrink()
+        {
+            Console.WriteLine("Available drinks:");
+            for (int index = 0; index < _factories.Count; index++)
+            {
+                var tuple = _factories[index];
+                Console.WriteLine($"{index}: {tuple.Item1}");
+            }
+
+            Console.WriteLine();
+
+            while (true)
+            {
+                string s;
+                if ((s = Console.ReadLine()) != null && int.TryParse(s, out int i) && i >= 0 && i < _factories.Count)
+                {
+                    Console.Write("Specify amount: ");
+                    s = Console.ReadLine();
+                    if (s != null && int.TryParse(s, out int amount) && amount > 0)
+                    {
+                        return _factories[i].Item2.Prepare(amount);
+                    }
+                }
+
+                Console.WriteLine("Incorret input, try again!");
+            }
+        }
     }
 
     internal static class Demo
     {
         private static void Main(string[] args)
         {
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink();
+            drink.Consume();
         }
     }
 }
