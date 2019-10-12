869be53536e080ed1bb1fbd4f9f35638a71d339b
Gledson Duarte <duarte.gledson@gmail.com>
2019-10-12 16:49:14 -0300

    Final example

--- Program.cs
+++ Program.cs
@@ -1,156 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using Autofac;
-using MoreLinq.Extensions;
-using NUnit.Framework;
+
 //using MoreLinq;
 
 namespace Console_BookExamples
 {
     //************* Singleton principle *************//
-    // A component which is instantiated only once 
-    public interface IDatabase
+    // Making a "safe" singleton is easy:
+    // construct a static Lazy<T> and return its Value
+    // Singletons are difficult to test
+    // Instead of directly using a singleton, consider depending on an abstraction (e.g. an interface)
+    // Consider defining singleton lifetime in DI container
+    internal class CEO
     {
-        int GetPopulation(string name);
-    }
+        private static string _name;
+        private static int _age;
 
-    public class SingleDatabase : IDatabase
-    {
-        private Dictionary<string, int> _capitals;
-        private static int _instanceCount;
-
-        public static int Count => _instanceCount;
-
-        private SingleDatabase()
+        public string Name
         {
-            _instanceCount++;
-            Console.WriteLine("Initalizing database");
-            _capitals = File.ReadAllLines(@"C:\Users\dg_gt\RiderProjects\Console_BookExamples\capitals.txt")
-                .Batch(2) // Comes from a external package "Nuget"
-                .ToDictionary(capital => capital.ElementAt(0).Trim(),
-                    population => int.Parse(population.ElementAt(1)));
+            get => _name;
+            set => _name = value;
         }
 
-        public int GetPopulation(string name)
+        public int Age
         {
-            return _capitals[name];
+            get => _age;
+            set => _age = value;
         }
 
-        private static Lazy<SingleDatabase> _instance = new Lazy<SingleDatabase>(() => new SingleDatabase());
-
-        public static SingleDatabase Instance => _instance.Value;
-    }
-
-    public class SingleTonRecordFinder
-    {
-        public int GetTotalPopulation(IEnumerable<string> names)
+        public override string ToString()
         {
-            int result = names.Sum(name => SingleDatabase.Instance.GetPopulation(name));
-            return result;
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
         }
     }
-
-    public class ConfigurableRecordFinder
-    {
-        private IDatabase _database;
-
-        public ConfigurableRecordFinder(IDatabase database)
-        {
-            _database = database;
-        }
-
-        public int GetTotalPopulation(IEnumerable<string> names)
-        {
-            int result = names.Sum(name => _database.GetPopulation(name));
-            return result;
-        }
-    }
-
-    public class DummyDatabase : IDatabase
-    {
-        public int GetPopulation(string name)
-        {
-            return new Dictionary<string, int>()
-            {
-                ["alpha"] = 1,
-                ["beta"] = 2,
-                ["gamma"] = 3
-            }[name];
-        }
-    }
-
-    public class OrdinaryDatabase: IDatabase
-    {
-        private Dictionary<string, int> _capitals;
-        public OrdinaryDatabase()
-        {
-            Console.WriteLine("Initalizing database");
-            _capitals = File.ReadAllLines(@"C:\Users\dg_gt\RiderProjects\Console_BookExamples\capitals.txt")
-                .Batch(2) // Comes from a external package "Nuget"
-                .ToDictionary(capital => capital.ElementAt(0).Trim(),
-                    population => int.Parse(population.ElementAt(1)));
-        }
-
-        public int GetPopulation(string name)
-        {
-            return _capitals[name];
-        }
-    }
-
-    [TestFixture]
-    public class SingletonTests
-    {
-        [Test]
-        public void IsSingleTonTest()
-        {
-            var db = SingleDatabase.Instance;
-            var db2 = SingleDatabase.Instance;
-            Assert.That(db, Is.SameAs(db2));
-            Assert.That(SingleDatabase.Count, Is.EqualTo(1));
-        }
-
-        [Test]
-        public void SingletonTotalPopulationTest()
-        {
-            var rf = new SingleTonRecordFinder();
-            var names = new[] {"Seoul", "Sao Paulo"};
-            int tp = rf.GetTotalPopulation(names);
-            Assert.That(tp, Is.EqualTo(17500000 + 17700000));
-        }
-
-        [Test]
-        public void ConfigurablePopulationTest()
-        {
-            var rf = new ConfigurableRecordFinder(new DummyDatabase());
-            var names = new[] {"alpha", "gamma"};
-            int tp = rf.GetTotalPopulation(names);
-            Assert.That(tp, Is.EqualTo(4));
-        }
-
-        [Test]
-        public void DIPopulationTest()
-        {
-            var cb = new ContainerBuilder();
-            cb.RegisterType<OrdinaryDatabase>().As<IDatabase>().SingleInstance();
-            cb.RegisterType<ConfigurableRecordFinder>();
-            using (var c = cb.Build())
-            {
-                var rf = c.Resolve<ConfigurableRecordFinder>();
-            }
-            
-        }
-        
-    }
+    
 
     internal static class Demo
     {
         private static void Main(string[] args)
         {
-            var db = SingleDatabase.Instance;
-            var city = "Tokyo";
-            Console.WriteLine($"{city} has population {db.GetPopulation(city)}");
+            var ceo = new CEO {Name = "Adam Smith", Age = 55};
+
+            var ceo2 = new CEO();
+            Console.WriteLine(ceo2);
         }
     }
 }
