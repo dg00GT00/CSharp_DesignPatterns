b801d2436b94ff681b62e9c3b5c3e9af84bb8e22
Gledson Duarte <duarte.gledson@gmail.com>
2019-10-20 08:31:07 -0300

    Final example

--- Program.cs
+++ Program.cs
@@ -1,17 +1,69 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Console_BookExamples
 {
-    //************* Single responsibility principle *************//
-    // A typical class is responsible for only one specific operation 
-    internal class Journal
+    //************* Composite principle *************//
+    // Objects can use other objects via inheritance/composition
+    // Some composed and singular objects need similar/identical behaviour
+    // Composite design pattern let us treat both types of objects uniformly
+    // A single object can masquerade as a collection with 'yield return this';
+
+    public static class ExtensionMethods
     {
+        public static void ConnectTo(this IEnumerable<Neuron> self, IEnumerable<Neuron> other)
+        {
+            if (ReferenceEquals(self, other))
+            {
+                return;
+            }
+
+            foreach (var from in self)
+            {
+                foreach (var to in other)
+                {
+                    from.Out.Add(to);
+                    to.In.Add(from);
+                }
+            }
+        }
+    }
+    
+    public class Neuron: IEnumerable<Neuron>
+    {
+        public float Value;
+        public List<Neuron> In, Out;
+        
+        public IEnumerator<Neuron> GetEnumerator()
+        {
+            yield return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
     }
 
+    public class NeuronLayer : Collection<Neuron>
+    {
+        
+    }
     internal static class Demo
     {
         private static void Main(string[] args)
         {
+            var neuron1 = new Neuron();
+            var neuron2 = new Neuron();
+            
+            neuron1.ConnectTo(neuron2);
+            
+            var layer1 = new NeuronLayer();
+            var layer2 = new NeuronLayer();
+            
+            neuron1.ConnectTo(layer1);
+            layer1.ConnectTo(layer2);
         }
     }
 }
