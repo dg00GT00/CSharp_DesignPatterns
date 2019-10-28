4e2de10bac95045fe38debecce2b5c67449ead31
Gledson Duarte <duarte.gledson@gmail.com>
2019-10-28 13:49:56 -0300

    Initial example

--- Program.cs
+++ Program.cs
@@ -2,16 +2,63 @@
 
 namespace Console_BookExamples
 {
-    //************* Single responsibility principle *************//
-    // A typical class is responsible for only one specific operation 
-    internal class Journal
+    //************* Proxy principle *************//
+    // A class that function as an interface to a particular resource.
+    // That resource may be remote, expensive to construct, or may require logging
+    // or some other added functionality
+
+    public interface ICar
     {
+        void Drive();
+    }
+
+    public class Car : ICar
+    {
+        public void Drive()
+        {
+            Console.WriteLine("Car is being driven");
+        }
+    }
+
+    public class Driver
+    {
+        public Driver(int age)
+        {
+            Age = age;
+        }
+
+        public int Age { get; set; }
+    }
+
+    public class CarProxy : ICar
+    {
+        private Driver _driver;
+        private Car _car = new Car();
+
+        public CarProxy(Driver driver)
+        {
+            _driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (_driver.Age >= 16)
+            {
+                _car.Drive();
+            }
+            else
+            {
+                Console.WriteLine("You are too young to driver the car");
+            }
+        }
     }
 
     internal static class Demo
     {
         private static void Main(string[] args)
         {
+            ICar car = new CarProxy(new Driver(12));
+            car.Drive();
         }
     }
 }
