e3668792ca945705e15bfddb4ef21af620ee3c7f
Gledson Duarte <duarte.gledson@gmail.com>
2019-10-28 16:10:28 -0300

    Composite Proxy: SoA/AoS

--- Program.cs
+++ Program.cs
@@ -7,66 +7,49 @@ namespace Console_BookExamples
     // A class that function as an interface to a particular resource.
     // That resource may be remote, expensive to construct, or may require logging
     // or some other added functionality
-    public struct Percentage : IEquatable<Percentage>
+
+    public class Creature
     {
-        private readonly float _value;
-
-        internal Percentage(float value)
-        {
-            _value = value;
-        }
-
-        public static float operator *(float f, Percentage p)
-        {
-            return f * p._value;
-        }
-
-        public static Percentage operator +(Percentage a, Percentage b)
-        {
-            return new Percentage(a._value + b._value);
-        }
-
-        public override string ToString()
-        {
-            return $"{_value * 100}%";
-        }
-
-        public bool Equals(Percentage other)
-        {
-            return _value.Equals(other._value);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return obj is Percentage other && Equals(other);
-        }
-
-        public override int GetHashCode()
-        {
-            return _value.GetHashCode();
-        }
-
-        public static bool operator ==(Percentage left, Percentage right)
-        {
-            return left.Equals(right);
-        }
-
-        public static bool operator !=(Percentage left, Percentage right)
-        {
-            return !left.Equals(right);
-        }
+        public byte Age;
+        public int X, Y;
     }
 
-    public static class PercentageExtensions
+    public class Creatures
     {
-        public static Percentage Percent(this int value)
+        private readonly int _size;
+        private byte[] _age;
+        private int[] _x, _y;
+
+        public Creatures(int size)
         {
-            return new Percentage(value / 100.00f);
+            _size = size;
+            _age = new byte[size];
+            _x = new int[size];
+            _y = new int[size];
         }
 
-        public static Percentage Percent(this float value)
+        public struct CreatureProxy
         {
-            return new Percentage(value / 100.00f);
+            private readonly Creatures _creatures;
+            private readonly int _index;
+
+            public CreatureProxy(Creatures creatures, int index)
+            {
+                _creatures = creatures;
+                _index = index;
+            }
+
+            public ref byte Age => ref _creatures._age[_index];
+            public ref int X => ref _creatures._x[_index];
+            public ref int Y => ref _creatures._y[_index];
+        }
+
+        public IEnumerator<CreatureProxy> GetEnumerator()
+        {
+            for (int pos = 0; pos < _size; pos++)
+            {
+                yield return new CreatureProxy(this, pos);
+            }
         }
     }
 
@@ -74,8 +57,11 @@ namespace Console_BookExamples
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine(10f * 5.Percent());
-            Console.WriteLine(2.Percent() + 3.Percent());
+            var creatures = new Creatures(100);
+            foreach (Creatures.CreatureProxy creature in creatures)
+            {
+                creature.X++;
+            }
         }
     }
 }
