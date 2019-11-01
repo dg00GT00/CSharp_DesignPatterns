d56fc5b539d0369e8d4b6e6886d7cac65d38ef87
Gledson Duarte <duarte.gledson@gmail.com>
2019-11-01 13:27:49 -0300

    Iterator Object example

--- Program.cs
+++ Program.cs
@@ -2,16 +2,102 @@
 
 namespace Console_BookExamples
 {
-    //************* Single responsibility principle *************//
-    // A typical class is responsible for only one specific operation 
-    internal class Journal
+    //********************** Iterator principle **********************//
+    // An object (or, in .NET, a method) that facilitates the traversal
+    // of a data structure
+
+    public class Node<T>
     {
+        public T Value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+            left.Parent = right.Parent = this;
+        }
+    }
+
+    public class InOrderIterator<T>
+    {
+        private readonly Node<T> _root;
+        public Node<T> Current;
+        private bool yieldedStart;
+
+        public InOrderIterator(Node<T> root)
+        {
+            _root = root;
+            Current = root;
+            while (Current.Left != null)
+            {
+                Current = Current.Left;
+            }
+        }
+
+        public bool MoveNext()
+        {
+            if (!yieldedStart)
+            {
+                yieldedStart = true;
+                return true;
+            }
+
+            if (Current.Right != null)
+            {
+                Current = Current.Right;
+                while (Current.Left != null)
+                {
+                    Current = Current.Left;
+                }
+
+                return true;
+            }
+            else
+            {
+                var p = Current.Parent;
+                while (p != null && Current == p.Right)
+                {
+                    Current = p;
+                    p = p.Parent;
+                }
+
+                Current = p;
+                return Current != null;
+            }
+        }
+
+        public void Reset()
+        {
+            Current = _root;
+            yieldedStart = false;
+        }
     }
 
     internal static class Demo
     {
         private static void Main(string[] args)
         {
+            //    1
+            //   / \
+            //  2   3
+            // in-order: 213
+            var root = new Node<int>(1, new Node<int>(2), new Node<int>(3));
+            var it = new InOrderIterator<int>(root);
+            while (it.MoveNext())
+            {
+                Console.Write(it.Current.Value);
+                Console.Write(", ");
+            }
+
+            Console.WriteLine();
         }
     }
 }
